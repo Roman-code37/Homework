f8bdd69d779c84d19e177742a58adfbdee6a829a
Roman <groma_92@mail.ru>
2019-09-18 09:06:10 +0300

    install commit

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,5 @@ public class Main {
         int volume = 8;
         int consumption = (distance / 100) * volume; //узнаем по формуле расход на 100км;
         System.out.println(consumption);
-
     }
 }
